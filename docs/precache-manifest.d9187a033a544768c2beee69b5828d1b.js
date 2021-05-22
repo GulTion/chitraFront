500f13b6c17e830c0e28b158bc7059ea32ee2664
GulTion <gkv8192@gmail.com>
2021-05-22 17:24:19 +0530

    version 0.0.1

---
+++ docs/precache-manifest.d9187a033a544768c2beee69b5828d1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcb88ce86027afc37427924df295d43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed61f38b8b0c3041b3f",
+    "url": "/static/css/main.dbee9e75.chunk.css"
+  },
+  {
+    "revision": "15b0423c301b1b74f52a",
+    "url": "/static/js/2.916b926e.chunk.js"
+  },
+  {
+    "revision": "5ed61f38b8b0c3041b3f",
+    "url": "/static/js/main.bee0dcaf.chunk.js"
+  },
+  {
+    "revision": "bc7615e31f6ee92c18d9",
+    "url": "/static/js/runtime-main.b33eb1b0.js"
+  }
+]);
