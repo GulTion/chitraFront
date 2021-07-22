1b68514eda2e88cedf2caa8917ae86ff572c3bca
GulTion <gkv8192@gmail.com>
2021-07-23 00:53:47 +0530

    Text implemented

--- src/ImagePersonalised/reducer.js
+++ src/ImagePersonalised/reducer.js
@@ -1,3 +1,9 @@
+import React from "react"
+import ArtBoardSettings from "./Components/ArtBoardSettings"
+import I from "./Components/Icons/"
+import uuid from "uuid-random"
+
+
 const istate = {
     canvas:{
         background:{
@@ -6,11 +12,18 @@ const istate = {
             value:"#ffffff",
             name:"#ffffff"
         }
+    },
+    objectList:[ 
+        {element:<ArtBoardSettings />, icon:I.artboard, isOpen:true, id:uuid(), title:"Artboad Settings", unique:true}
+    ],
+    textSettings:{
+        
     }
 }
 const {log} = console
 
 export default (state =istate, action) => {
+    const {data, type} = action;
     switch (action.type) {
      case 'COLOR_CHOOSE_CANVAS':
          log(action)
@@ -20,7 +33,27 @@ export default (state =istate, action) => {
            ...state.canvas,
            background:action.data
        }
-      }
+      };    
+      case 'ADD_TEXT_OBJECT':
+          log(data)
+          return {
+              ...state,
+              objectList:[data, ...state.objectList]
+             
+          }  
+      case 'OBJECT_LIST_CLOSE':
+          return {
+              ...state,
+              objectList:state.objectList.map(_=>{
+                if(_.id==action.data.id){
+                    return {..._, ...action.data}
+                }
+                return {..._, isOpen:false}
+            })
+          };
+
+        
+     
      default:
       return state
     }
