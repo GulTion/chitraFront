0175b04d66674ffdc91d56a109f634c0bac639a0
GulTion <gkv8192@gmail.com>
2021-08-08 21:07:47 +0530

    updated

---
+++ docs/build/precache-manifest.2025a6814b4f0e4de669e7ae4101a81e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746cbd9f11df0638c564853113b36200",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b346f907fdbcc5ef32a",
+    "url": "/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "4ce957275888c3171d8c",
+    "url": "/static/css/main.008c81f3.chunk.css"
+  },
+  {
+    "revision": "3b346f907fdbcc5ef32a",
+    "url": "/static/js/2.c503ba80.chunk.js"
+  },
+  {
+    "revision": "4ce957275888c3171d8c",
+    "url": "/static/js/main.8fcf1d10.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  },
+  {
+    "revision": "d2648682a1e8221b39a7a1ac5952448f",
+    "url": "/static/media/pen.d2648682.svg"
+  }
+]);
